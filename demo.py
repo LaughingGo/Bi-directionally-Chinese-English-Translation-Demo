293a91083c3b33b66bb1d19c6b4718d1bf7b8159
Jianfeng Huang <1053692971@qq.com>
2021-02-21 13:03:31 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,9 +9,9 @@ src_text = [
 ]
 
 # model_zh2en = 'Helsinki-NLP/opus-mt-zh-en'
-model_zh2en = 'opus-mt-zh-en'  # pre-download from https://mirrors.tuna.tsinghua.edu.cn/hugging-face-models/Helsinki-NLP/
-tokenizer_zh2en = MarianTokenizer.from_pretrained(model_zh2en)
-model = MarianMTModel.from_pretrained(model_zh2en)
+model_name = 'opus-mt-zh-en'  # pre-download from https://mirrors.tuna.tsinghua.edu.cn/hugging-face-models/Helsinki-NLP/
+tokenizer_zh2en = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
 inputs = tokenizer_zh2en(src_text, padding=True, return_tensors="pt")
 translate = model.generate(**inputs, max_length=128)
 trans = [tokenizer_zh2en.decode(ids, skip_special_tokens=True) for ids in translate]
@@ -23,9 +23,9 @@ src_text = [
     'love me, love my dog.'
 ]
 # model_en2zh = 'Helsinki-NLP/opus-mt-en-zh'
-model_en2zh = 'opus-mt-en-zh' # pre-download from https://mirrors.tuna.tsinghua.edu.cn/hugging-face-models/Helsinki-NLP/
-tokenizer_en2zh = MarianTokenizer.from_pretrained(model_en2zh)
-model = MarianMTModel.from_pretrained(model_en2zh)
+model_name = 'opus-mt-en-zh' # pre-download from https://mirrors.tuna.tsinghua.edu.cn/hugging-face-models/Helsinki-NLP/
+tokenizer_en2zh = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
 
 
 inputs = tokenizer_en2zh(src_text, padding=True, return_tensors="pt")
