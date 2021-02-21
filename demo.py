b8a69ec715860136aea926ec55d3f5f4efc0e9d1
Jianfeng Huang <1053692971@qq.com>
2021-02-21 13:00:17 +0800

    Update demo.py
    Add the URL for download the pretrained model by offline.

--- demo.py
+++ demo.py
@@ -1,11 +1,15 @@
 import torch
 from transformers import MarianMTModel, MarianTokenizer, AutoTokenizer, AutoModelWithLMHead
+
+
+
+## translate Chinese to English
 src_text = [
     '爱屋及乌。'
 ]
 
 # model_zh2en = 'Helsinki-NLP/opus-mt-zh-en'
-model_zh2en = 'opus-mt-zh-en'
+model_zh2en = 'opus-mt-zh-en'  # pre-download from https://mirrors.tuna.tsinghua.edu.cn/hugging-face-models/Helsinki-NLP/
 tokenizer_zh2en = MarianTokenizer.from_pretrained(model_zh2en)
 model = MarianMTModel.from_pretrained(model_zh2en)
 inputs = tokenizer_zh2en(src_text, padding=True, return_tensors="pt")
@@ -13,11 +17,13 @@ translate = model.generate(**inputs, max_length=128)
 trans = [tokenizer_zh2en.decode(ids, skip_special_tokens=True) for ids in translate]
 print(trans[0])
 
+
+## translate English to Chinese
 src_text = [
     'love me, love my dog.'
 ]
 # model_en2zh = 'Helsinki-NLP/opus-mt-en-zh'
-model_en2zh = 'opus-mt-en-zh'
+model_en2zh = 'opus-mt-en-zh' # pre-download from https://mirrors.tuna.tsinghua.edu.cn/hugging-face-models/Helsinki-NLP/
 tokenizer_en2zh = MarianTokenizer.from_pretrained(model_en2zh)
 model = MarianMTModel.from_pretrained(model_en2zh)
 
